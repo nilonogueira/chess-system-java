8a17fd83f5279647b9991969b4cd61bb8e87b400
Nilo <nogueira.nilo@hotmail.com>
2020-03-15 11:55:14 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessException extends RuntimeException{
+	private static final long serialVensionUID = 1L;
+	
+	public ChessException(String msg) {
+		super(msg);
+	}
+	
+
+}
